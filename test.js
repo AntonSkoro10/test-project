d0570a849c344c3ee4e9555ecf3faf124e46e835
AntonSkoro10 <antonskoro112@gmail.com>
2023-12-16 12:14:18 +0200

    Update test.js

--- test.js
+++ test.js
@@ -95,4 +95,38 @@
 // console.log(getSubscriptionPrice("random"));       // повертає рядок "Invalid subscription type!"
 // console.log(getSubscriptionPrice("premium"));      // повертає рядок "Invalid subscription type!"
 
+// TASK-1 //
 
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
+//     const totalPrice = quantity * pricePerDroid;
+//     if (totalPrice > customerCredits) {
+//         return "Insufficient funds!";
+//     } else {
+//         return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+//     }
+// }
+
+// console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
+//     // Розрахунок загальної суми замовлення
+//     const totalPrice = quantity * pricePerDroid;
+
+//     // Перевірка на достатність коштів
+//     if (totalPrice > customerCredits) {
+//         return "Insufficient funds!";
+//     } else {
+//         return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+//     }
+// }
+
+// // Перевірка функції
+// console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
