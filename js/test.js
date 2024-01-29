aecddd88e39ce37cb11527bc62a62b5637cfac71
AntonSkoro10 <antonskoro112@gmail.com>
2024-01-29 17:48:32 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -418,30 +418,362 @@
 // }
 // console.log(getIndexElement(["Mango", "Poly", "Ajax"], "Poly"));
 
-function slugify(title) {
-    const toLowerCasedTitle = title.toLowerCase()
-    const words = toLowerCasedTitle.split(' ')
-    const slug = words.join('-')
-    return slug
+// function slugify(title) {
+//     const toLowerCasedTitle = title.toLowerCase()
+//     const words = toLowerCasedTitle.split(' ')
+//     const slug = words.join('-')
+//     return slug
+// }
+
+// console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+// console.log(slugify("English for developer")); // "english-for-developer"
+// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//     const allArray = firstArray.concat(secondArray)
+//     if (allArray > maxLength) {
+//         return allArray.slice(0, maxLength)
+//     } else {
+//         return allArray
+//     }
+// }
+
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+
+// const elements = ['rusvelt', 'iosif', 'cherchel']
+// elements.push('adolf')
+// console.log(elements)
+
+// function createArrayForNumbers(min, max) {
+//     const numbers = [];
+//     for (let i = min; i <= max; i ++) {
+//         numbers.push(i);
+//     }
+//     return numbers;
+// }
+
+// console.log(createArrayForNumbers(1, 3));
+// console.log(createArrayForNumbers(14, 17));
+// console.log(createArrayForNumbers(20, 30));
+
+// const elements = ['rusvelt', 'iosif', 'cherchel']
+// for (let i = 0; elements.length > i; i ++) {
+//     console.log(elements[i]);
+// }
+
+// function calculateTotalPrice(order) {
+//     let total = 0;
+//     for (let i = 0; i < order.length; i ++) {
+//         total += order[i];
+//     }
+//     return total
+// }
+// console.log(calculateTotalPrice([164, 188, 121]));
+
+// function getEvenNumber(start, end) {
+//     let numbers = []
+//     for (let i = start; i <= end; i++) {
+//         if (i % 2 === 0) {
+//             numbers.push(i)
+//         }
+//     }
+//     return numbers
+// }
+ 
+// console.log(getEvenNumber(5, 8));
+
+// function filterArray(numbers, value) {
+//     let newArray = []
+//     for (let i = 0; i < numbers.length; i ++) {
+//         if (numbers[i] > value) {
+//             newArray.push(numbers[i])
+//         }
+//     }
+//     return newArray
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+// function checkStorage(storage, item) {
+//     const lowerCaseItem = item.toLowerCase();
+//     for (let i = 0; i < storage.length; i ++) {
+//         const lowerCaseItemStorage = storage[i].toLowerCase();
+//         if (lowerCaseItemStorage === lowerCaseItem) {
+//             return `${item} is available to order!`;
+//         }
+//     }
+//     return "Sorry! We are out of stock!";
+// }
+
+// console.log(checkStorage(["apple", "plum", "pear"], "orange"));
+// console.log(checkStorage(["apple", "plum", "pear"], "apple"));
+// console.log(checkStorage(["apple", "plum", "pear"], "plum"));
+// console.log(checkStorage(["apple", "plum", "pear"], "pear"));
+// console.log(checkStorage(["apple", "plum", "pear"], "banana"));
+
+// function foo() {
+//     const args = Array.from(arguments)
+//     return args.join("-")
+// }
+
+// console.log (foo(1,2,3))
+
+// const fcBarcelona = {
+//     coach: "Xavi Hernandez",
+//     captain: "Sergi Roberto",
+//     stadium: "Spotify Camp Nou",
+
+//     trofyes: {
+//         championsLeage: 5,
+//         laLiga: 26,
+//         superCopa: 14,
+//         copdDelRey: 31,
+//         superCupUefa: 5,
+//     },
+
+//     history: {
+//         bestPlayerHistory: "Lionel Messi",
+//         bestStrikerHistory: "Lionel Messi",
+//     },
+
+//     sponsors: {
+//         title: "spotify",
+//         general: "nike",
+//     },
+
+//     bestPlayersNow: {
+//         bestPlayerNow: "Frenkie de Jong",
+//         bestStrikerLastSeason: "Robert Lewandowski",
+//     },
+
+//     derby:  ["Real Madrid", "Atletico Madrid", "Athletic", "Girona", "Espanyol"]
+
+// };
+
+// const keys = Object.keys(fcBarcelona);
+
+// for (const key of keys) {
+//   console.log(key + ": " + fcBarcelona[key]);
+
+
+
+// const keys = ["coach", "captain", "stadium"]
+// const values = ["Xavi Hernandez", "Sergi Roberto", "Spotify Camp Nou"]
+
+// for (const keys in fcBarcelona) {
+//   console.log(keys)
+//   console.log(fcBarcelona[values])
+// }
+
+
+
+// const trofyes = fcBarcelona.trofyes;
+// const history = fcBarcelona.history;
+// const sponsors = fcBarcelona.sponsors;
+// const bestPlayersNow = fcBarcelona.bestPlayersNow;
+// const derby = fcBarcelona.derby;
+
+// console.log(fcBarcelona);
+// console.log(trofyes);
+// console.log(history);
+// console.log(sponsors);
+// console.log(bestPlayersNow);
+// console.log(derby);
+
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+  
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// apartment.are = 60;
+// apartment.room = 3;
+// apartment.location = {
+//   counrty: "Jamaica",
+//   city: "Kingston",
+// };
+
+
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = ["descr", "rating", "price"];
+// const values = ["Spacious apartment in the city center", 4, 2153];
+
+// for (const keys in apartment) {
+//   console.log(keys)
+//   console.log(apartment[values])
+// }
+
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'genres', 'rating']
+
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+
+//   return null;
+// }
+
+// console.log(getProductPrice("Radar"));
+
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 1 },
+//     { name: "Grip", price: 1200, quantity: 2 },
+//     { name: "Scanner", price: 2700, quantity: 1 },
+//     { name: "Droid", price: 400, quantity: 4 },
+//   ];
+
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+
+//   // Якщо продукт з такою назвою не знайдений
+//   return null;
+// }
+
+// // Приклади викликів функції:
+// console.log(getProductPrice("Radar"));    // Виведе 1300
+// console.log(getProductPrice("Grip"));     // Виведе 1200
+// console.log(getProductPrice("Scanner"));  // Виведе 2700
+// console.log(getProductPrice("Droid"));    // Виведе 400
+// console.log(getProductPrice("Engine"));   // Виведе null
+
+
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 1 },
+//     { name: "Grip", price: 1200, quantity: 2 },
+//     { name: "Scanner", price: 2700, quantity: 1 },
+//     { name: "Droid", price: 400, quantity: 4 },
+//   ];
+
+//   for (const product of products) {
+//     if (product.name === productName) {
+//        return product.price
+//     }
+//   }
+//     return null
+// }
+
+// console.log(getProductPrice("Radar"));    // Виведе 1300
+// console.log(getProductPrice("Grip"));     // Виведе 1200
+// console.log(getProductPrice("Scanner"));  // Виведе 2700
+// console.log(getProductPrice("Droid"));    // Виведе 400
+// console.log(getProductPrice("Engine"));   // Виведе null
+
+// const gadgetsShelf = {
+//     gadgets: [
+//         { brend: "apple", type: "smartphone" },
+//         { brend: "apple", type: "headphone" },
+//     ],
+
+//     changeModel(newName, newType) {
+        
+//         for (const gadget of this.gadgets) {
+//             if (gadget.brend === newName) {
+//                 gadget.model = newType
+//             }
+//         }
+//     }
+// };
+
+// gadgetsShelf.changeModel("Samsung", "smart watch");
+
+// console.log(gadgetsShelf.gadgets);
+
+// function multiply(...args) {
+//     console.log(args)
+// }
+ 
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+const getUserNames = (users) => {
+  return users.map(user => user.name);
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-
-function makeArray(firstArray, secondArray, maxLength) {
-    const allArray = firstArray.concat(secondArray)
-    if (allArray > maxLength) {
-        return allArray.slice(0, maxLength)
-    } else {
-        return allArray
-    }
-}
-
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+console.log(
+  getUserNames([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      balance: 2811
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      balance: 3821
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      balance: 3793
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      balance: 2278
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      balance: 3951
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      balance: 1498
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      balance: 2764
+    },
+  ])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
